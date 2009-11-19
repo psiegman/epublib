813efb75f967db2927932a7f5c07cd56685f5a25
Paul Siegmann <paul.siegmann+github@gmail.com>
2009-11-19 09:36:32 +0100

    work on writing document

--- src/main/java/nl/siegmann/epublib/PackageDocument.java
+++ src/main/java/nl/siegmann/epublib/PackageDocument.java
@@ -5,21 +5,53 @@ import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamWriter;
 
 public class PackageDocument {
-	public static final String NAMESPACE_NCX = "http://www.idpf.org/2007/opf";
+	public static final String NAMESPACE_OPF = "http://www.idpf.org/2007/opf";
+	public static final String NAMESPACE_DUBLIN_CORE = "http://purl.org/dc/elements/1.1/";
+	public static final String PREFIX_DUBLIN_CORE = "dc";
 	
 	public static void write(XMLEventFactory eventFactory, XMLStreamWriter writer, Book book) throws XMLStreamException {
 		writer.writeStartDocument(Constants.encoding, "1.0");
-		writer.setDefaultNamespace(NAMESPACE_NCX);
-		writer.writeStartElement(NAMESPACE_NCX, "ncx");
+		writer.setDefaultNamespace(NAMESPACE_OPF);
+		writer.writeNamespace(PREFIX_DUBLIN_CORE, NAMESPACE_DUBLIN_CORE);
+		writer.writeStartElement(NAMESPACE_OPF, "package");
 //		writer.writeNamespace("ncx", NAMESPACE_NCX);
-		writer.writeAttribute("xmlns", NAMESPACE_NCX);
-		writer.writeAttribute("version", "2005-1");
-		writer.writeStartElement(NAMESPACE_NCX, "head");
+		writer.writeAttribute("xmlns", NAMESPACE_OPF);
+		writer.writeAttribute("version", "2.0");
+		writer.writeAttribute("unique-identifier", "BookID");
 
-		writer.writeStartElement(NAMESPACE_NCX, "meta");
+		writer.writeStartElement(NAMESPACE_OPF, "metadata");
+		
+		writer.writeStartElement(NAMESPACE_DUBLIN_CORE, "identifier");
+		writer.writeAttribute(NAMESPACE_DUBLIN_CORE, "id", "BookdID");
+		writer.writeAttribute(NAMESPACE_OPF, "scheme", "UUID");
+		writer.writeCharacters(book.getUid());
+		writer.writeEndElement(); // dc:identifier
+
+		writer.writeStartElement(NAMESPACE_DUBLIN_CORE, "title");
+		writer.writeCharacters(book.getTitle());
+		writer.writeEndElement(); // dc:title
+
+		for(Author author: book.getAuthors()) {
+			writer.writeStartElement(NAMESPACE_DUBLIN_CORE, "creator");
+			writer.writeAttribute(NAMESPACE_OPF, "role", "aut");
+			writer.writeAttribute(NAMESPACE_OPF, "file-as", author.getLastname() + ", " + author.getFirstname());
+			writer.writeCharacters(author.getFirstname() + " " + author.getLastname());
+			writer.writeEndElement(); // dc:creator
+		}
+
+		for(String subject: book.getSubjects()) {
+			writer.writeStartElement(NAMESPACE_DUBLIN_CORE, "subject");
+			writer.writeCharacters(subject);
+			writer.writeEndElement(); // dc:subject
+		}
+
+		writer.writeEndElement(); // dc:metadata
+
+		writer.writeStartElement(NAMESPACE_OPF, "meta");
 		writer.writeAttribute("name", "dtb:uid");
 		writer.writeAttribute("content", book.getUid());
-		writer.writeEndElement();
+
+		writer.writeEndElement(); // package
 	}
 		/*
  
