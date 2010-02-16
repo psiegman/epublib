c0c121b74fb9601178b0e7dcfc2d0600b1e89f5b
Paul Siegmann <paul.siegmann+github@gmail.com>
2010-02-16 22:23:12 +0100

    docs work done on the htmlsplitterbookprocessor. Got stuck

--- src/main/java/nl/siegmann/epublib/bookprocessor/BookProcessorUtil.java
+++ src/main/java/nl/siegmann/epublib/bookprocessor/BookProcessorUtil.java
@@ -3,10 +3,23 @@ package nl.siegmann.epublib.bookprocessor;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.apache.commons.lang.StringUtils;
+
 import nl.siegmann.epublib.domain.Book;
 import nl.siegmann.epublib.domain.Resource;
 
 public class BookProcessorUtil {
+
+	/**
+	 * From the href it takes the part before '#' or the whole href otherwise and uses that as the key to find the resource in the resource map.
+	 * 
+	 * @param href
+	 * @param resources
+	 * @return
+	 */
+	public static Resource getResourceByHref(String href, Map<String, Resource> resources) {
+		return resources.get(StringUtils.substringBefore(href, "#"));
+	}
 
 	/**
 	 * Creates a map with as key the href of the resource and as value the Resource.
