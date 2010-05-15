1dfadd7c6768f01c452e102d17aefb2719ee3089
Paul Siegmann <paul@zoot.(none)>
2010-05-15 12:26:42 +0200

    add package-info

---
+++ src/main/java/nl/siegmann/epublib/bookprocessor/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * The classes in this package are used for post-processing Books.
+ * Things like cleaning up the html, adding a cover page, etc.
+ */
+package nl.siegmann.epublib.bookprocessor;
