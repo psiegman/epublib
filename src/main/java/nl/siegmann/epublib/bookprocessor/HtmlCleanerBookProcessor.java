c46f62c587374fcb73a16b6a81ce73923de01711
Paul Siegmann <paul@zoot.(none)>
2010-05-16 22:49:35 +0200

    change encoding handling

--- src/main/java/nl/siegmann/epublib/bookprocessor/HtmlCleanerBookProcessor.java
+++ src/main/java/nl/siegmann/epublib/bookprocessor/HtmlCleanerBookProcessor.java
@@ -6,12 +6,14 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.charset.Charset;
 
+import nl.siegmann.epublib.Constants;
 import nl.siegmann.epublib.domain.Book;
 import nl.siegmann.epublib.domain.Resource;
 import nl.siegmann.epublib.epub.EpubWriter;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
+import org.htmlcleaner.CleanerProperties;
 import org.htmlcleaner.HtmlCleaner;
 import org.htmlcleaner.SimpleXmlSerializer;
 import org.htmlcleaner.TagNode;
@@ -29,7 +31,6 @@ public class HtmlCleanerBookProcessor extends HtmlBookProcessor implements BookP
 	@SuppressWarnings("unused")
 	private final static Logger log = Logger.getLogger(HtmlCleanerBookProcessor.class);
 	
-	public static final String OUTPUT_ENCODING = "UTF-8";
 	private HtmlCleaner htmlCleaner;
 	private XmlSerializer newXmlSerializer;
 
@@ -40,22 +41,26 @@ public class HtmlCleanerBookProcessor extends HtmlBookProcessor implements BookP
 
 	private static HtmlCleaner createHtmlCleaner() {
 		HtmlCleaner result = new HtmlCleaner();
-//		CleanerProperties cleanerProperties = result.getProperties();
+		CleanerProperties cleanerProperties = result.getProperties();
 //		cleanerProperties.setTranslateSpecialEntities(false);
+		cleanerProperties.setNamespacesAware(true);
+		cleanerProperties.setOmitDoctypeDeclaration(false);
 		return result;
 	}
 	
 
-	public byte[] processHtml(Resource resource, Book book, EpubWriter epubWriter) throws IOException {
+	@SuppressWarnings("unchecked")
+	public byte[] processHtml(Resource resource, Book book, EpubWriter epubWriter, String encoding) throws IOException {
 		Reader reader;
-		if(! StringUtils.isEmpty(resource.getInputEncoding())) {
+		if(StringUtils.isNotBlank(resource.getInputEncoding())) {
 			reader = new InputStreamReader(resource.getInputStream(), Charset.forName(resource.getInputEncoding()));
 		} else {
 			reader = new InputStreamReader(resource.getInputStream());
 		}
 		TagNode node = htmlCleaner.clean(reader);
+		node.getAttributes().put("xmlns", Constants.NAMESPACE_XHTML);
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		newXmlSerializer.writeXmlToStream(node, out, OUTPUT_ENCODING);
+		newXmlSerializer.writeXmlToStream(node, out, encoding);
 		return out.toByteArray();
 	}
 }
