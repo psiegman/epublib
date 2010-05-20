31af8dbc75f38e6e3376c4cf7833e7ecab914ae9
Paul Siegmann <paul.siegmann+github@gmail.com>
2010-05-20 20:05:03 +0200

    add EPUBLib generator name

--- src/main/java/nl/siegmann/epublib/Constants.java
+++ src/main/java/nl/siegmann/epublib/Constants.java
@@ -4,4 +4,5 @@ package nl.siegmann.epublib;
 public interface Constants {
 	String ENCODING = "UTF-8";
 	String NAMESPACE_XHTML = "http://www.w3.org/1999/xhtml";
+	String EPUBLIB_GENERATOR_NAME = "EPUBLib version 0.1";
 }
