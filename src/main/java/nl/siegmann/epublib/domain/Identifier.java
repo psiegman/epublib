da7d527843c59956dd152d069e2e6c51b610c872
Paul Siegmann <paul.siegmann+github@gmail.com>
2010-05-20 20:19:34 +0200

    allow for multiple identifiers

--- src/main/java/nl/siegmann/epublib/domain/Identifier.java
+++ src/main/java/nl/siegmann/epublib/domain/Identifier.java
@@ -1,5 +1,6 @@
 package nl.siegmann.epublib.domain;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -17,6 +18,7 @@ public class Identifier {
 		String URL = "URL";
 	}
 	
+	private boolean bookId = false;
 	private String scheme;
 	private String value;
 
@@ -33,6 +35,32 @@ public class Identifier {
 		this.value = value;
 	}
 
+	/**
+	 * The first identifier for which the bookId is true is made the bookId identifier.
+	 * If no identifier has bookId == true then the first bookId identifier is written as the primary.
+	 * 
+	 * @param identifiers
+	 * @return
+	 */
+	public static Identifier getBookIdIdentifier(List<Identifier> identifiers) {
+		if(identifiers == null || identifiers.isEmpty()) {
+			return null;
+		}
+		
+		Identifier result = null;
+		for(Identifier identifier: identifiers) {
+			if(identifier.isBookId()) {
+				result = identifier;
+				break;
+			}
+		}
+		
+		if(result == null) {
+			result = identifiers.get(0);
+		}
+		
+		return result;
+	}
 	
 	public String getScheme() {
 		return scheme;
@@ -45,5 +73,23 @@ public class Identifier {
 	}
 	public void setValue(String value) {
 		this.value = value;
+	}
+
+
+	public void setBookId(boolean bookId) {
+		this.bookId = bookId;
+	}
+
+
+	/**
+	 * This bookId property allows the book creator to add multiple ids and tell the epubwriter which one to write out as the bookId.
+	 *  
+	 * The Dublin Core metadata spec allows multiple identifiers for a Book.
+	 * The epub spec requires exactly one identifier to be marked as the book id.
+	 * 
+	 * @return
+	 */
+	public boolean isBookId() {
+		return bookId;
 	}
 }
