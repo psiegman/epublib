c2564bbcd815063b0d411ad04b80e3217aa05e1c
Paul Siegmann <paul.siegmann+github@gmail.com>
2010-05-29 22:32:30 +0200

    fix xpath test

--- src/test/java/nl/siegmann/epublib/epub/XPathTest.java
+++ src/test/java/nl/siegmann/epublib/epub/XPathTest.java
@@ -1,7 +1,7 @@
 package nl.siegmann.epublib.epub;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -11,9 +11,9 @@ import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
 import junit.framework.TestCase;
+import nl.siegmann.epublib.utilities.HtmlSplitterTest;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
@@ -21,19 +21,16 @@ public class XPathTest extends TestCase {
 	
 	public void test1() {
 		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+		documentBuilderFactory.setNamespaceAware(true);
 		try {
-			Document document = documentBuilderFactory.newDocumentBuilder().parse(new FileInputStream("/home/paul/ncx.xml"));
+			InputStream input = HtmlSplitterTest.class.getResourceAsStream("/toc.xml");
+			Document ncxDocument = documentBuilderFactory.newDocumentBuilder().parse(input);
 			XPathFactory factory = XPathFactory.newInstance();
-		    XPath xpath = factory.newXPath();
-		    NodeList nodes = (NodeList) xpath.evaluate("ncx/navMap/navPoint", document, XPathConstants.NODESET);
-//		    System.out.println("found ncxnode:" + ncxNode.getLocalName());
-//		    XPath xpath2 = factory.newXPath();
-//		    NodeList nodes = (NodeList) result;
-		    for (int i = 0; i < nodes.getLength(); i++) {
-		        System.out.println(((Element) nodes.item(i)).getAttribute("id")); 
-		        String label = xpath.evaluate("navLabel/text", nodes.item(i));
-		        System.out.println("label:" + label);
-		    }
+			XPath xpath = factory.newXPath();
+			xpath.setNamespaceContext(NCXDocument.NCX_DOC_NAMESPACE_CONTEXT);
+			final String PREFIX_NCX = "ncx";
+		    NodeList navmapNodes = (NodeList) xpath.evaluate("/" + PREFIX_NCX + ":ncx/" + PREFIX_NCX + ":navMap/" + PREFIX_NCX + ":navPoint", ncxDocument, XPathConstants.NODESET);
+		    assertEquals(3, navmapNodes.getLength());
 		} catch (SAXException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
