456e848d83bece3aa718fada23724297fa6992fd
Paul Siegmann <paul.siegmann+github@gmail.com>
2010-05-29 22:36:56 +0200

    add NULL_RESOURCE

--- src/main/java/nl/siegmann/epublib/domain/Resource.java
+++ src/main/java/nl/siegmann/epublib/domain/Resource.java
@@ -11,10 +11,68 @@ import java.io.InputStream;
  *
  */
 public interface Resource {
+	Resource NULL_RESOURCE = new Resource() {
+
+		@Override
+		public String getHref() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public String getId() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public String getInputEncoding() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public InputStream getInputStream() throws IOException {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public MediaType getMediaType() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public void setHref(String href) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void setId(String id) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void setInputEncoding(String encoding) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void setMediaType(MediaType mediaType) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	};
 	void setId(String id);
 	String getId();
 	String getInputEncoding();
 	String getHref();
+	void setInputEncoding(String encoding);
 	void setHref(String href);
 	MediaType getMediaType();
 	void setMediaType(MediaType mediaType);
