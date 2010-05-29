512ad6a8afaad5763e404f792b6e5713b9eb1c40
Paul Siegmann <paul.siegmann+github@gmail.com>
2010-05-29 22:40:28 +0200

    add new htmlSplitter test

--- src/test/java/nl/siegmann/epublib/utilities/HtmlSplitterTest.java
+++ src/test/java/nl/siegmann/epublib/utilities/HtmlSplitterTest.java
@@ -1,9 +1,8 @@
 package nl.siegmann.epublib.utilities;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.List;
 
@@ -13,24 +12,29 @@ import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.XMLEvent;
 
 import junit.framework.TestCase;
+import nl.siegmann.epublib.Constants;
 
 public class HtmlSplitterTest extends TestCase {
 
-	public void Xtest1() {
+	public void test1() {
 		HtmlSplitter htmlSplitter = new HtmlSplitter();
 		try {
-			Reader input = new FileReader("/home/paul/anathem.xhtml");
-			List<List<XMLEvent>> result = htmlSplitter.splitHtml(input, 10000);
-			System.out.println(this.getClass().getName() + ": split in " + result.size() + " pieces");
+			String bookResourceName = "/moby_dick.html";
+			Reader input = new InputStreamReader(HtmlSplitterTest.class.getResourceAsStream(bookResourceName), Constants.ENCODING);
+			int maxSize = 10000;
+			List<List<XMLEvent>> result = htmlSplitter.splitHtml(input, maxSize);
+//			System.out.println(this.getClass().getName() + ": split in " + result.size() + " pieces");
 			XMLOutputFactory xmlOutputFactory = XMLOutputFactory.newInstance();
 			for(int i = 0; i < result.size(); i++) {
-				File partFile = new File("part_" + i + ".xhtml");
-				XMLEventWriter out = xmlOutputFactory.createXMLEventWriter(new FileWriter(partFile));
+				ByteArrayOutputStream out = new ByteArrayOutputStream();
+				XMLEventWriter writer = xmlOutputFactory.createXMLEventWriter(out);
 				for(XMLEvent xmlEvent: result.get(i)) {
-					out.add(xmlEvent);
+					writer.add(xmlEvent);
 				}
-				out.close();
-				System.out.println("written to " + partFile.getAbsolutePath());
+				writer.close();
+				byte[] data = out.toByteArray();
+				assertTrue(data.length > 0);
+				assertTrue(data.length <= maxSize);
 			}
 		} catch (XMLStreamException e) {
 			// TODO Auto-generated catch block
