aeac74192a085504df13ff20fb5e94ec6a710cc5
Paul Siegmann <paul.siegmann+github@gmail.com>
2010-06-17 22:01:05 +0200

    fix bug allow for adding of Resources without href

--- src/main/java/nl/siegmann/epublib/domain/Resources.java
+++ src/main/java/nl/siegmann/epublib/domain/Resources.java
@@ -4,15 +4,47 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import nl.siegmann.epublib.service.MediatypeService;
+
+import org.apache.commons.lang.StringUtils;
+
 public class Resources {
 
 	private Map<String, Resource> resources = new HashMap<String, Resource>();
 	
 	public Resource add(Resource resource) {
+		fixHref(resource);
 		this.resources.put(resource.getHref(), resource);
 		return resource;
 	}
 
+	private void fixHref(Resource resource) {
+		if(! StringUtils.isBlank(resource.getHref())
+				&& ! resources.containsKey(resource.getHref())) {
+			return;
+		}
+		if(StringUtils.isBlank(resource.getHref())) {
+			if(resource.getMediaType() == null) {
+				throw new IllegalArgumentException("Resource must have either a MediaType or a href");
+			}
+			int i = 1;
+			String href = createHref(resource.getMediaType(), i);
+			while(resources.containsKey(href)) {
+				href = createHref(resource.getMediaType(), (++i));
+			}
+			resource.setHref(href);
+		}
+	}
+	
+	private String createHref(MediaType mediaType, int counter) {
+		if(MediatypeService.isBitmapImage(mediaType)) {
+			return "image_" + counter + mediaType.getDefaultExtension();
+		} else {
+			return "item_" + counter + mediaType.getDefaultExtension();
+		}
+	}
+	
+	
 	public boolean isEmpty() {
 		return resources.isEmpty();
 	}
@@ -41,12 +73,13 @@ public class Resources {
 	}
 	
 	public void set(Collection<Resource> resources) {
-		resources.clear();
+		this.resources.clear();
 		addAll(resources);
 	}
 	
 	public void addAll(Collection<Resource> resources) {
 		for(Resource resource: resources) {
+			fixHref(resource);
 			this.resources.put(resource.getHref(), resource);
 		}
 	}
@@ -59,5 +92,4 @@ public class Resources {
 		Resource result = resources.get(href);
 		return result;
 	}
-
 }
