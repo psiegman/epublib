76919797b17910afd3e112aac60d3adef320aba2
Paul Siegmann <paul.siegmann+github@gmail.com>
2010-06-17 22:02:22 +0200

    add test

---
+++ src/test/java/nl/siegmann/epublib/epub/FilesetBookCreatorTest.java
@@ -0,0 +1,31 @@
+package nl.siegmann.epublib.epub;
+
+import junit.framework.TestCase;
+import nl.siegmann.epublib.domain.Book;
+import nl.siegmann.epublib.fileset.FilesetBookCreator;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.vfs.FileObject;
+import org.apache.commons.vfs.FileSystemManager;
+import org.apache.commons.vfs.NameScope;
+import org.apache.commons.vfs.VFS;
+
+public class FilesetBookCreatorTest extends TestCase {
+
+	public void test1() {
+		try {
+			FileSystemManager fsManager = VFS.getManager();
+			FileObject dir = fsManager.resolveFile("ram://test-dir");
+			dir.createFolder();
+			FileObject chapter1 = dir.resolveFile("chapter1.html", NameScope.CHILD);
+			chapter1.createFile();
+			IOUtils.copy(this.getClass().getResourceAsStream("/book1/chapter1.html"), chapter1.getContent().getOutputStream());
+			Book bookFromDirectory = FilesetBookCreator.createBookFromDirectory(dir, "UTF-8");
+			assertEquals(1, bookFromDirectory.getResources().size());
+			assertEquals(1, bookFromDirectory.getSpineSections().size());
+			assertEquals(1, bookFromDirectory.getTocSections().size());
+		} catch(Exception e) {
+			assertTrue(false);
+		}
+	}
+}
