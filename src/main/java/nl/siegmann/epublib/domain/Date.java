d31ac9cdfca5f9121ebb1376e4b3219280e91617
Paul Siegmann <paul.siegmann+github@gmail.com>
2010-07-21 20:29:12 +0200

    add Date

---
+++ src/main/java/nl/siegmann/epublib/domain/Date.java
@@ -0,0 +1,71 @@
+package nl.siegmann.epublib.domain;
+
+import java.text.SimpleDateFormat;
+
+import nl.siegmann.epublib.epub.PackageDocumentBase;
+
+public class Date {
+	public enum Event {
+		PUBLICATION("publication"), CREATION("creation");
+		
+		private final String value;
+
+		Event(String v) {
+			value = v;
+		}
+
+		public static Event fromValue(String v) {
+			for (Event c : Event.values()) {
+				if (c.value.equals(v)) {
+					return c;
+				}
+			}
+			return null;
+		}
+		
+		public String toString() {
+			return value;
+		}
+	};
+
+	private Event event;
+	private String dateString;
+
+	public Date(java.util.Date date) {
+		this(date, (Event) null);
+	}
+	
+	public Date(String dateString) {
+		this(dateString, (Event) null);
+	}
+	
+	public Date(java.util.Date date, Event event) {
+		this((new SimpleDateFormat(PackageDocumentBase.dateFormat)).format(date), event);
+	}
+	
+	public Date(String dateString, Event event) {
+		this.dateString = dateString;
+		this.event = event;
+	}
+	
+	public Date(java.util.Date date, String event) {
+		this((new SimpleDateFormat(PackageDocumentBase.dateFormat)).format(date), event);
+	}
+	
+	public Date(String dateString, String event) {
+		this(dateString, Event.fromValue(event));
+		this.dateString = dateString;
+	}
+
+	public String getValue() {
+		return dateString;
+	}
+	public Event getEvent() {
+		return event;
+	}
+	
+	public void setEvent(Event event) {
+		this.event = event;
+	}
+}
+
