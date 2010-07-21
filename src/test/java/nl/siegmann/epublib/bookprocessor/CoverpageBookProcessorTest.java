2271cd1d359dc1b5b83afeecfe1d0462b542e7e0
Paul Siegmann <paul.siegmann+github@gmail.com>
2010-07-21 20:36:56 +0200

    add more tests

--- src/test/java/nl/siegmann/epublib/bookprocessor/CoverpageBookProcessorTest.java
+++ src/test/java/nl/siegmann/epublib/bookprocessor/CoverpageBookProcessorTest.java
@@ -15,4 +15,18 @@ public class CoverpageBookProcessorTest extends TestCase {
 			assertEquals(testData[i + 2], actualResult);
 		}
 	}
+
+	public void testCollapsePathDots() {
+		String[] testData = new String[] {
+				"/foo/bar.html", "/foo/bar.html",
+				"/foo/../bar.html", "/bar.html",
+				"/foo//bar.html", "/foo/bar.html",
+				"/foo/./bar.html", "/foo/bar.html",
+				"/foo/../sub/bar.html", "/sub/bar.html"
+		};
+		for (int i = 0; i < testData.length; i += 2) {
+			String actualResult = CoverpageBookProcessor.collapsePathDots(testData[i]);
+			assertEquals(testData[i + 1], actualResult);
+		}
+	}
 }
