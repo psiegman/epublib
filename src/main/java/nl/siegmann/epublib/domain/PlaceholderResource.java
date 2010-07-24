81dd32c0e63031e9b9b74c12a247572da06e1526
Paul Siegmann <paul.siegmann+github@gmail.com>
2010-07-24 23:28:42 +0200

    Big refactoring: every Section now has a Resource instead of a href to a resource

---
+++ src/main/java/nl/siegmann/epublib/domain/PlaceholderResource.java
@@ -0,0 +1,24 @@
+package nl.siegmann.epublib.domain;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * A Resource that is useful during parsing: It will hold the href to the later to be retrieved real resource.
+ * 
+ * @author paul
+ *
+ */
+public class PlaceholderResource extends ResourceBase implements Resource {
+
+	public PlaceholderResource(String href) {
+		super(href);
+	}
+
+	@Override
+	public InputStream getInputStream() throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
