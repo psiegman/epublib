3fc170cd6c7e9982a09a17c1dfe0dd8c35c00942
Paul Siegmann <paul.siegmann+github@gmail.com>
2010-11-14 20:30:01 +0100

    make tests work again after file rename

--- src/test/java/nl/siegmann/epublib/bookprocessor/CoverpageBookProcessorTest.java
+++ src/test/java/nl/siegmann/epublib/bookprocessor/CoverpageBookProcessorTest.java
@@ -2,7 +2,7 @@ package nl.siegmann.epublib.bookprocessor;
 
 import junit.framework.TestCase;
 
-public class CoverpageBookProcessorTest extends TestCase {
+public class CoverPageBookProcessorTest extends TestCase {
 
 	public void testCalculateAbsoluteImageHref1() {
 		String[] testData = new String[] {
@@ -16,17 +16,4 @@ public class CoverpageBookProcessorTest extends TestCase {
 		}
 	}
 
-	public void testCollapsePathDots() {
-		String[] testData = new String[] {
-				"/foo/bar.html", "/foo/bar.html",
-				"/foo/../bar.html", "/bar.html",
-				"/foo//bar.html", "/foo/bar.html",
-				"/foo/./bar.html", "/foo/bar.html",
-				"/foo/../sub/bar.html", "/sub/bar.html"
-		};
-		for (int i = 0; i < testData.length; i += 2) {
-			String actualResult = CoverpageBookProcessor.collapsePathDots(testData[i]);
-			assertEquals(testData[i + 1], actualResult);
-		}
-	}
 }
