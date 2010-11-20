663e3affb03e4c540dd4e69d3e2dfd61be0b7829
Paul Siegmann <paul.siegmann+github@gmail.com>
2010-11-20 17:59:08 +0100

    improve resource content reading

--- src/main/java/nl/siegmann/epublib/util/ResourceUtil.java
+++ src/main/java/nl/siegmann/epublib/util/ResourceUtil.java
@@ -1,6 +1,7 @@
 package nl.siegmann.epublib.util;
 
 import java.io.IOException;
+import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -8,6 +9,7 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import nl.siegmann.epublib.domain.Resource;
 
+import org.slf4j.Logger;import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
@@ -21,6 +23,44 @@ import com.google.gdata.util.io.base.UnicodeReader;
  */
 public class ResourceUtil {
 	
+	private static Logger log = LoggerFactory.getLogger(ResourceUtil.class);
+	
+	/**
+	 * Gets the contents of the Resource as Reader.
+	 * 
+	 * Does all sorts of smart things (courtesy of commons io XmlStreamReader) to handle encodings, byte order markers, etc.
+	 * 
+	 * @see http://commons.apache.org/io/api-release/org/apache/commons/io/input/XmlStreamReader.html
+	 * 
+	 * @param resource
+	 * @return
+	 * @throws IOException
+	 */
+	public static Reader getReader(Resource resource) throws IOException  {
+		if (resource == null) {
+			log.error("null resource passed to getReader");
+			return null;
+		}
+//		XmlStreamReader xmlStreamReader = new XmlStreamReader(resource.getInputStream(), true, resource.getInputEncoding().name());
+//		IOUtils.copy(xmlStreamReader, System.out);
+//		xmlStreamReader = new XmlStreamReader(resource.getInputStream(), true, resource.getInputEncoding().name());
+//		return xmlStreamReader;
+		return new UnicodeReader(resource.getInputStream(), resource.getInputEncoding().name());
+	}
+	
+	/**
+	 * Gets the contents of the Resource as an InputSource
+	 */
+	public static InputSource getInputSource(Resource resource) throws IOException {
+		Reader reader = getReader(resource);
+		if (reader == null) {
+			return null;
+		}
+		InputSource inputSource = new InputSource(reader);
+		return inputSource;
+	}
+	
+	
 	/**
 	 * Reads the given resources inputstream, parses the xml therein and returns the result as a Document
 	 * @param resource
@@ -32,8 +72,10 @@ public class ResourceUtil {
 	 * @throws ParserConfigurationException
 	 */
 	public static Document getAsDocument(Resource resource, DocumentBuilder documentBuilder) throws UnsupportedEncodingException, SAXException, IOException, ParserConfigurationException {
-		UnicodeReader unicodeReader = new UnicodeReader(resource.getInputStream(), resource.getInputEncoding().name());
-		InputSource inputSource = new InputSource(unicodeReader);
+		InputSource inputSource = getInputSource(resource);
+		if (inputSource == null) {
+			return null;
+		}
 		Document result = documentBuilder.parse(inputSource);
 		result.setXmlStandalone(true);
 		return result;
