6dcdf4fa2ba133290554907928b5f1f6139684f4
Paul Siegmann <paul.siegmann+github@gmail.com>
2010-11-20 17:59:28 +0100

    show cover image in metadata pane

--- src/main/java/nl/siegmann/epublib/viewer/MetadataPane.java
+++ src/main/java/nl/siegmann/epublib/viewer/MetadataPane.java
@@ -1,9 +1,15 @@
 package nl.siegmann.epublib.viewer;
 
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -12,23 +18,49 @@ import javax.swing.table.TableModel;
 
 import nl.siegmann.epublib.browsersupport.Navigator;
 import nl.siegmann.epublib.domain.Metadata;
+import nl.siegmann.epublib.domain.Resource;
 
 import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MetadataPane extends JPanel {
 
+	private static final Logger log = LoggerFactory.getLogger(MetadataPane.class);
+	
 	private static final long serialVersionUID = -2810193923996466948L;
 
 	public MetadataPane(Navigator navigator) {
-		super(new GridLayout(1, 0));
+		super(new GridLayout(0, 1));
 		JTable table = new JTable(
 				createTableData(navigator.getBook().getMetadata()),
 				new String[] {"", ""});
         table.setFillsViewportHeight(true);
-        JScrollPane scrollPane = new JScrollPane(table);
+        JPanel contentPanel = new JPanel(new BorderLayout());
+        JScrollPane scrollPane = new JScrollPane(contentPanel);
+        contentPanel.add(table, BorderLayout.CENTER);
+        setCoverImage(contentPanel, navigator);
 		this.add(scrollPane);
 	}
 
+	private void setCoverImage(JPanel contentPanel, Navigator navigator) {
+		Resource coverImageResource = navigator.getBook().getCoverImage();
+		if (coverImageResource == null) {
+			System.out.println("no cover image");
+			return;
+		}
+		try {
+			Image image = ImageIO.read(coverImageResource.getInputStream());
+			image = image.getScaledInstance(200, -1, Image.SCALE_SMOOTH);
+			System.out.println("added cover image");
+			JLabel label = new JLabel(new ImageIcon(image));
+			label.setSize(100, 100);
+			contentPanel.add(label, BorderLayout.NORTH);
+		} catch (IOException e) {
+			log.error("Unable to load cover image from book", e.getMessage());
+		}
+	}
+	
 	private Object[][] createTableData(Metadata metadata) {
 		List<String[]> result = new ArrayList<String[]>();
 		addStrings(metadata.getIdentifiers(), "Identifier", result);
