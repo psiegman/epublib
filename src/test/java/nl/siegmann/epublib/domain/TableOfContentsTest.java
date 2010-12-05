c217e41815270df284bd0ecf540558cf31c4f668
Paul Siegmann <paul.siegmann+github@gmail.com>
2010-12-05 21:11:31 +0100

    add addResourceAtLocation method to TableOfContents

--- src/test/java/nl/siegmann/epublib/domain/TableOfContentsTest.java
+++ src/test/java/nl/siegmann/epublib/domain/TableOfContentsTest.java
@@ -24,4 +24,36 @@ public class TableOfContentsTest extends TestCase {
 		
 		assertEquals(2, tableOfContents.calculateDepth());
 	}
+	
+	public void testAddAtLocation1() {
+		Resource resource = new Resource("foo/bar");
+		TableOfContents toc = new TableOfContents();
+		TOCReference tocReference = toc.addResourceAtLocation(resource, "apple/pear", "/");
+		assertNotNull(tocReference);
+		assertNotNull(tocReference.getResource());
+		assertEquals(2, toc.size());
+		assertEquals("pear", tocReference.getTitle());
+	}
+
+	public void testAddAtLocation2() {
+		Resource resource = new Resource("foo/bar");
+		TableOfContents toc = new TableOfContents();
+		TOCReference tocReference = toc.addResourceAtLocation(resource, "apple/pear", "/");
+		assertNotNull(tocReference);
+		assertNotNull(tocReference.getResource());
+		assertEquals(2, toc.size());
+		assertEquals("pear", tocReference.getTitle());
+
+		TOCReference tocReference2 = toc.addResourceAtLocation(resource, "apple/banana", "/");
+		assertNotNull(tocReference2);
+		assertNotNull(tocReference2.getResource());
+		assertEquals(3, toc.size());
+		assertEquals("banana", tocReference2.getTitle());
+
+		TOCReference tocReference3 = toc.addResourceAtLocation(resource, "apple", "/");
+		assertNotNull(tocReference3);
+		assertNotNull(tocReference.getResource());
+		assertEquals(3, toc.size());
+		assertEquals("apple", tocReference3.getTitle());
+	}
 }
