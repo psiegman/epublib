bce27c3321094bfcf59f7c266c72530ac0f03a04
Paul Siegmann <paul.siegmann+github@gmail.com>
2010-12-20 22:56:33 +0100

    log error when icon not found

--- src/main/java/nl/siegmann/epublib/viewer/ViewerUtil.java
+++ src/main/java/nl/siegmann/epublib/viewer/ViewerUtil.java
@@ -6,7 +6,12 @@ import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ViewerUtil {
+	
+	private static Logger log = LoggerFactory.getLogger(ViewerUtil.class);
 
 	/**
 	 * Creates a button with the given icon. The icon will be loaded from the classpath.
@@ -31,10 +36,12 @@ public class ViewerUtil {
 	
 	static ImageIcon createImageIcon(String iconName) {
 		ImageIcon result = null;
+		String fullIconPath = "/viewer/icons/" + iconName + ".png";
 		try {
-			Image image = ImageIO.read(ViewerUtil.class.getResourceAsStream("/viewer/icons/" + iconName + ".png"));
+			Image image = ImageIO.read(ViewerUtil.class.getResourceAsStream(fullIconPath));
 			result = new ImageIcon(image);
 		} catch(Exception e) {
+			log.error("Icon \'" + fullIconPath + "\' not found");
 		}
 		return result;
 	}
