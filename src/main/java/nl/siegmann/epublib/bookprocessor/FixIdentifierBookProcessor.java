b640b1c0da2f085ea1d45d3907ef7ee9dd78cf3e
psiegman <paul.siegmann+github@gmail.com>
2011-05-07 23:45:33 +0200

    reduce the number of dependencies to reduce download size

--- src/main/java/nl/siegmann/epublib/bookprocessor/FixIdentifierBookProcessor.java
+++ src/main/java/nl/siegmann/epublib/bookprocessor/FixIdentifierBookProcessor.java
@@ -4,8 +4,6 @@ import nl.siegmann.epublib.domain.Book;
 import nl.siegmann.epublib.domain.Identifier;
 import nl.siegmann.epublib.epub.EpubProcessor;
 
-import org.apache.commons.collections.CollectionUtils;
-
 /**
  * If the book has no identifier it adds a generated UUID as identifier.
  * 
@@ -16,7 +14,7 @@ public class FixIdentifierBookProcessor implements BookProcessor {
 
 	@Override
 	public Book processBook(Book book, EpubProcessor epubProcessor) {
-		if(CollectionUtils.isEmpty(book.getMetadata().getIdentifiers())) {
+		if(book.getMetadata().getIdentifiers().isEmpty()) {
 			book.getMetadata().addIdentifier(new Identifier());
 		}
 		return book;
