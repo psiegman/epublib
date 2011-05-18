a38216ee3267ea039f45f99c0d8dce7b635fa96d
psiegman <paul.siegmann+github@gmail.com>
2011-05-18 23:04:48 +0200

    bump version in epublib generator name

--- epublib-core/src/main/java/nl/siegmann/epublib/Constants.java
+++ epublib-core/src/main/java/nl/siegmann/epublib/Constants.java
@@ -5,7 +5,7 @@ package nl.siegmann.epublib;
 public interface Constants {
 	String ENCODING = "UTF-8";
 	String NAMESPACE_XHTML = "http://www.w3.org/1999/xhtml";
-	String EPUBLIB_GENERATOR_NAME = "EPUBLib version 1.1";
+	String EPUBLIB_GENERATOR_NAME = "EPUBLib version 3.0";
 	char FRAGMENT_SEPARATOR_CHAR = '#';
 	String DEFAULT_TOC_ID = "toc";
 }
