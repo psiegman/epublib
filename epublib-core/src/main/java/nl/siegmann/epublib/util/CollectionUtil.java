eb53543a21a67f231ad26b2ec3ad4d6116c8b598
psiegman <paul.siegmann+github@gmail.com>
2011-07-03 21:44:36 +0200

    make the viewer less memory-hungry by only loading the images of the current section and discarding them when the user moves to another section

--- epublib-core/src/main/java/nl/siegmann/epublib/util/CollectionUtil.java
+++ epublib-core/src/main/java/nl/siegmann/epublib/util/CollectionUtil.java
@@ -1,9 +1,53 @@
 package nl.siegmann.epublib.util;
 
+import java.util.Enumeration;
+import java.util.Iterator;
 import java.util.List;
 
 public class CollectionUtil {
 
+	/**
+	 * Wraps an Enumeration around an Iterator
+	 * @author paul.siegmann
+	 *
+	 * @param <T>
+	 */
+	private static class IteratorEnumerationAdapter<T> implements Enumeration<T> {
+		private Iterator<T> iterator;
+
+		public IteratorEnumerationAdapter(Iterator<T> iter) {
+			this.iterator = iter;
+		}
+		
+		@Override
+		public boolean hasMoreElements() {
+			return iterator.hasNext();
+		}
+
+		@Override
+		public T nextElement() {
+			return iterator.next();
+		}
+	}
+	
+	/**
+	 * Creates an Enumeration out of the given Iterator.
+	 * @param <T>
+	 * @param it
+	 * @return
+	 */
+	public static <T> Enumeration<T> createEnumerationFromIterator(Iterator<T> it) {
+		return new IteratorEnumerationAdapter<T>(it);
+	}
+	
+	
+	/**
+	 * Returns the first element of the list, null if the list is null or empty.
+	 * 
+	 * @param <T>
+	 * @param list
+	 * @return
+	 */
 	public static <T> T first(List<T> list) {
 		if(list == null || list.isEmpty()) {
 			return null;
