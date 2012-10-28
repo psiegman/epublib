d564b6dcebfb301226fa383944f189fc97a0a29d
Paul Siegmann <paul.siegmann+github@gmail.com>
2012-10-28 15:00:25 +0100

    apply lukepfarrar changes to XslBookProcessor
    remove debug printing

--- epublib-tools/src/main/java/nl/siegmann/epublib/bookprocessor/XslBookProcessor.java
+++ epublib-tools/src/main/java/nl/siegmann/epublib/bookprocessor/XslBookProcessor.java
@@ -2,26 +2,34 @@ package nl.siegmann.epublib.bookprocessor;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.Result;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
 import nl.siegmann.epublib.domain.Book;
 import nl.siegmann.epublib.domain.Resource;
 import nl.siegmann.epublib.epub.BookProcessor;
+import nl.siegmann.epublib.epub.EpubProcessorSupport;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 
 /**
@@ -44,17 +52,28 @@ public class XslBookProcessor extends HtmlBookProcessor implements BookProcessor
 
 	@Override
 	public byte[] processHtml(Resource resource, Book book, String encoding) throws IOException {
-		Source htmlSource = new StreamSource(new InputStreamReader(resource.getInputStream(), resource.getInputEncoding()));
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		Writer writer = new OutputStreamWriter(out,encoding);
-		Result streamResult = new StreamResult(writer);
+		byte[] result = null;
 		try {
-			transformer.transform(htmlSource, streamResult);
-		} catch (TransformerException e) {
-			log.error(e.getMessage(), e);
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	    DocumentBuilder db = dbFactory.newDocumentBuilder();
+	    db.setEntityResolver(EpubProcessorSupport.getEntityResolver());
+
+	    Document doc = db.parse(new InputSource(resource.getReader()));
+	    
+	    Source htmlSource = new DOMSource(doc.getDocumentElement());
+	    ByteArrayOutputStream out = new ByteArrayOutputStream();
+	    Writer writer = new OutputStreamWriter(out, "UTF-8");
+	    Result streamResult = new StreamResult(writer);
+	    try {
+	    	transformer.transform(htmlSource, streamResult);
+	    } catch (TransformerException e) {
+	    	log.error(e.getMessage(), e);
+	    	throw new IOException(e);
+	    }
+	    result = out.toByteArray();
+	    return result;
+		} catch (Exception e) {
 			throw new IOException(e);
 		}
-		byte[] result = out.toByteArray();
-		return result;
 	}
 }
