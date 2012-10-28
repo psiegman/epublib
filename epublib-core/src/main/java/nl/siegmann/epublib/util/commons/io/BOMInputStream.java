e88f2f001b6e5b38880ea8413e7ab04f8e2e3e20
Paul Siegmann <paul.siegmann+github@gmail.com>
2012-10-28 14:53:28 +0100

    copy/paste commons-io 2.4 XmlStreamReader into epublib

--- epublib-core/src/main/java/nl/siegmann/epublib/util/commons/io/BOMInputStream.java
+++ epublib-core/src/main/java/nl/siegmann/epublib/util/commons/io/BOMInputStream.java
@@ -1,5 +1,3 @@
-package nl.siegmann.epublib.util.commons.io;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -16,12 +14,14 @@ package nl.siegmann.epublib.util.commons.io;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package nl.siegmann.epublib.util.commons.io;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.io.ByteOrderMark;
 
 /**
  * This class is used to wrap a stream that includes an encoded
