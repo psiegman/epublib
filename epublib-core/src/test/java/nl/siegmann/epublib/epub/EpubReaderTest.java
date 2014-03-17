df6a846cbdc2485d58278f28c966732b0467ca23
P. Siegmann <paul.siegmann@gmail.com>
2014-03-17 05:27:01 +0100

    modernize unit test style

--- epublib-core/src/test/java/nl/siegmann/epublib/epub/EpubReaderTest.java
+++ epublib-core/src/test/java/nl/siegmann/epublib/epub/EpubReaderTest.java
@@ -2,77 +2,73 @@ package nl.siegmann.epublib.epub;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
-import junit.framework.TestCase;
 import nl.siegmann.epublib.domain.Book;
 import nl.siegmann.epublib.domain.Resource;
 import nl.siegmann.epublib.service.MediatypeService;
 
-public class EpubReaderTest extends TestCase {
-	
-	public void testCover_only_cover() {
-		try {
-			Book book = new Book();
-			
-			book.setCoverImage(new Resource(this.getClass().getResourceAsStream("/book1/cover.png"), "cover.png"));
+import org.junit.Assert;
+import org.junit.Test;
 
-			ByteArrayOutputStream out = new ByteArrayOutputStream();
-			(new EpubWriter()).write(book, out);
-			byte[] epubData = out.toByteArray();
-			Book readBook = new EpubReader().readEpub(new ByteArrayInputStream(epubData));
-			assertNotNull(readBook.getCoverImage());
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			assertTrue(false);
-		}
+public class EpubReaderTest {
 
+	@Test
+	public void testCover_only_cover() throws IOException {
+		Book book = new Book();
+
+		book.setCoverImage(new Resource(this.getClass().getResourceAsStream(
+				"/book1/cover.png"), "cover.png"));
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		(new EpubWriter()).write(book, out);
+		byte[] epubData = out.toByteArray();
+		Book readBook = new EpubReader().readEpub(new ByteArrayInputStream(
+				epubData));
+		Assert.assertNotNull(readBook.getCoverImage());
 	}
 
-	public void testCover_cover_one_section() {
-		try {
-			Book book = new Book();
-			
-			book.setCoverImage(new Resource(this.getClass().getResourceAsStream("/book1/cover.png"), "cover.png"));
-			book.addSection("Introduction", new Resource(this.getClass().getResourceAsStream("/book1/chapter1.html"), "chapter1.html"));
-			book.generateSpineFromTableOfContents();
-			
-			ByteArrayOutputStream out = new ByteArrayOutputStream();
-			(new EpubWriter()).write(book, out);
-			byte[] epubData = out.toByteArray();
-			Book readBook = new EpubReader().readEpub(new ByteArrayInputStream(epubData));
-			assertNotNull(readBook.getCoverPage());
-			assertEquals(1, readBook.getSpine().size());
-			assertEquals(1, readBook.getTableOfContents().size());
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			assertTrue(false);
-		}
+	@Test
+	public void testCover_cover_one_section() throws IOException {
+		Book book = new Book();
+
+		book.setCoverImage(new Resource(this.getClass().getResourceAsStream(
+				"/book1/cover.png"), "cover.png"));
+		book.addSection("Introduction", new Resource(this.getClass()
+				.getResourceAsStream("/book1/chapter1.html"), "chapter1.html"));
+		book.generateSpineFromTableOfContents();
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		(new EpubWriter()).write(book, out);
+		byte[] epubData = out.toByteArray();
+		Book readBook = new EpubReader().readEpub(new ByteArrayInputStream(
+				epubData));
+		Assert.assertNotNull(readBook.getCoverPage());
+		Assert.assertEquals(1, readBook.getSpine().size());
+		Assert.assertEquals(1, readBook.getTableOfContents().size());
 	}
 
-	public void testReadEpub_opf_ncx_docs() {
-		try {
-			Book book = new Book();
-			
-			book.setCoverImage(new Resource(this.getClass().getResourceAsStream("/book1/cover.png"), "cover.png"));
-			book.addSection("Introduction", new Resource(this.getClass().getResourceAsStream("/book1/chapter1.html"), "chapter1.html"));
-			book.generateSpineFromTableOfContents();
-			
-			ByteArrayOutputStream out = new ByteArrayOutputStream();
-			(new EpubWriter()).write(book, out);
-			byte[] epubData = out.toByteArray();
-			Book readBook = new EpubReader().readEpub(new ByteArrayInputStream(epubData));
-			assertNotNull(readBook.getCoverPage());
-			assertEquals(1, readBook.getSpine().size());
-			assertEquals(1, readBook.getTableOfContents().size());
-			assertNotNull(readBook.getOpfResource());
-			assertNotNull(readBook.getNcxResource());
-			assertEquals(MediatypeService.NCX, readBook.getNcxResource().getMediaType());
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			assertTrue(false);
-		}
+	@Test
+	public void testReadEpub_opf_ncx_docs() throws IOException {
+		Book book = new Book();
+
+		book.setCoverImage(new Resource(this.getClass().getResourceAsStream(
+				"/book1/cover.png"), "cover.png"));
+		book.addSection("Introduction", new Resource(this.getClass()
+				.getResourceAsStream("/book1/chapter1.html"), "chapter1.html"));
+		book.generateSpineFromTableOfContents();
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		(new EpubWriter()).write(book, out);
+		byte[] epubData = out.toByteArray();
+		Book readBook = new EpubReader().readEpub(new ByteArrayInputStream(
+				epubData));
+		Assert.assertNotNull(readBook.getCoverPage());
+		Assert.assertEquals(1, readBook.getSpine().size());
+		Assert.assertEquals(1, readBook.getTableOfContents().size());
+		Assert.assertNotNull(readBook.getOpfResource());
+		Assert.assertNotNull(readBook.getNcxResource());
+		Assert.assertEquals(MediatypeService.NCX, readBook.getNcxResource()
+				.getMediaType());
 	}
 }
