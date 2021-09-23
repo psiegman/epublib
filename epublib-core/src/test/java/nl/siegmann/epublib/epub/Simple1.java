8b10c35088c2b5a6f66035b57259807273d145a4
Christian Hauf <christian.hauf@kambrium.net>
2021-09-23 10:02:42 +0200

    Fixed distribution management config Fixed broken Testcase

--- epublib-core/src/test/java/nl/siegmann/epublib/epub/Simple1.java
+++ epublib-core/src/test/java/nl/siegmann/epublib/epub/Simple1.java
@@ -2,10 +2,7 @@ package nl.siegmann.epublib.epub;
 
 import java.io.FileOutputStream;
 
-import nl.siegmann.epublib.domain.Author;
-import nl.siegmann.epublib.domain.Book;
-import nl.siegmann.epublib.domain.Resource;
-import nl.siegmann.epublib.domain.TOCReference;
+import nl.siegmann.epublib.domain.*;
 
 public class Simple1 {
     public static void main(String[] args) {
@@ -14,7 +11,7 @@ public class Simple1 {
             Book book = new Book();
 
             // Set the title
-            book.getMetadata().addTitle("Epublib test book 1");
+            book.getMetadata().addTitle(new Title("Epublib test book 1"));
 
             // Add an Author
             book.getMetadata().addAuthor(new Author("Joe", "Tester"));
